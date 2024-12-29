02b466a4c3a89c036fc9a43c37f64bf1f16f3b04
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2024-12-29 10:26:52 +0600

    Add files via upload

--- CodeBook/src/pages/Products.jsx
+++ CodeBook/src/pages/Products.jsx
@@ -1,12 +1,48 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
 import Card from '../components/Card'
+import { CiMenuKebab } from 'react-icons/ci'
+
+import getProductList from '../apiServices/ProductServices'
 
 const Products = () => {
+
+const[products, setProducts] = useState()
+console.log(products)
+
+  useEffect(()=>{
+     async function fetchAllproduct(){
+      try{
+        const productsData = await getProductList();
+        setProducts(productsData);
+      }
+      catch(error){
+        alert(error.message)
+      }
+     }
+     fetchAllproduct()
+  },[])
+
   return (
-    <div className='bg-red-500 h-[100vh]'>
-        <Card/>
-    </div>
+    <section>
+      <div className='header flex justify-between p-10 font-bold'>
+        <h1>All eBooks(15)</h1>
+
+        <p className='bg-gray-200 p-3 rounded-md'>
+          <CiMenuKebab/>
+        </p>
+
+      </div>
+      <div className='grid gap-4 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 p-10'>
+        {products?.map((product) => (
+          <article key={product.id}>
+            <Card product={product}/>
+          </article>
+        )
+        
+        )}
+      </div>
+    </section>
   )
 }
 
