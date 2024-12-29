b213723b2803ef674521111230a4363541ddb67e
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2024-12-29 13:30:38 +0600

    Update ProductServices.jsx

--- CodeBook/src/apiServices/ProductServices.jsx
+++ CodeBook/src/apiServices/ProductServices.jsx
@@ -1,10 +1,23 @@
-/* eslint-disable no-unused-vars */
 //data fetch kora start
  const getProductList = async()=>{
-    const productList = await fetch("http://localhost:8000/products")
+    const productList = await fetch("http://localhost:800/products")
 
     const data = await productList.json()
     return data;
 }
 export default getProductList
 // data fetch end
+
+//Upcomingproduct fetch start
+export const getUpcomingProducts = async()=>{
+    const response = await fetch("http://localhost:800/upcoming_products")
+    const data = await response.json()
+    return data;
+}
+//Upcomingproduct fetch end
+
+export const getProductDetails = async(id)=>{
+    const response = await fetch(`http://localhost:800/upcoming_products/${id}`)
+    const data = await response.json()
+    return data;
+}
