b831baf4caf1b73c132a9aa3dc4e99d1beba9e9a
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2024-12-29 13:36:06 +0600

    Update FeaturedeBooks.jsx

--- CodeBook/src/components/featuredeBooks/FeaturedeBooks.jsx
+++ CodeBook/src/components/featuredeBooks/FeaturedeBooks.jsx
@@ -1,66 +1,30 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-key */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Card'
 import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { getUpcomingProducts } from '../../apiServices/ProductServices';
 
-const products = [
-  {
-    id:1,
-    name: "Basics to Advanced in React",
-    overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-    rating: 5,
-    best_seller: true,
-    price: "$99"
-  },
-  {
-    id:2,
-    name: "Django Framework for Beginners",
-    overview: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
-    rating: 4,
-    best_seller: false,
-    price: "$100"
-  },
-  {
-    id:3,
-    name: "The Future of Design Systems",
-    overview: "Contrary to popular belief, Lorem Ipsum is not simply random text.",
-    rating : 3,
-    best_seller: true,
-    price:"$98"
-  },
-  {
-    id:4,
-    name: "Basics to Advanced in React",
-    overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-    rating: 5,
-    best_seller: true,
-    price: "$99"
-  },
-  {
-    id:5,
-    name: "Basics to Advanced in React",
-    overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-    rating: 5,
-    best_seller: true,
-    price: "$99"
-  },
-  {
-    id:6,
-    name: "Basics to Advanced in React",
-    overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-    rating: 5,
-    best_seller: true,
-    price: "$99"
-  },
-]
 
 const FeaturedeBooks = () => {
   
-  const [productsList, setProductsList]=useState(products)
+  const [productsList, setProductsList]=useState()
+
+  useEffect(()=>{
+    async function fetchUpcommingProduct(){
+      try{
+        const ucProduct = await getUpcomingProducts();
+        setProductsList(ucProduct)
+      }
+      catch(error){
+        alert(error)
+      }
+    }
+    fetchUpcommingProduct()
+  },[])
 
   var settings = {
     dots: false,
@@ -102,7 +66,7 @@ const FeaturedeBooks = () => {
   return (
     <section>
         <div className='p-10'>
-            <h2 className='text-center text-[30px] font-bold text-black underline underline-offset-4'>Featured eBooks</h2>
+            <h2 className='text-center text-[30px] font-bold text-black underline underline-offset-4'>Upcoming eBooks</h2>
             
             <br /><br />
             <div className=' '>
@@ -110,7 +74,7 @@ const FeaturedeBooks = () => {
 
 
               {
-                productsList.map((product)=>(
+                productsList?.map((product)=>(
                   <article>
                     <Card product={product}/>
                   </article> 
