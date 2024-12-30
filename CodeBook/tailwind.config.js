5f088bc06a77f23c88a67de5bda8bca99795f508
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2024-12-30 11:58:32 +0600

    Update tailwind.config.js

--- CodeBook/tailwind.config.js
+++ CodeBook/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        dark: "#1E293B",
+      }
+    },
   },
   plugins: [],
+  darkMode: 'class',
 }
