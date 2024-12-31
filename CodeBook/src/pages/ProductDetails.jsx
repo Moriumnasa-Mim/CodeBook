544dad7289c683a28b49baf29e9eb4bf3fc12ac7
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2024-12-31 12:52:44 +0600

    Update ProductDetails.jsx

--- CodeBook/src/pages/ProductDetails.jsx
+++ CodeBook/src/pages/ProductDetails.jsx
@@ -28,27 +28,39 @@ const ProductDetails = () => {
 
   return (
     
-    <section>
+    <section className='dark:text-slate-300'> 
         
-        <div className="px-5 py-8 md:p-10">
-            <div className="top flex flex-col gap-5">
-                <h2 className='text-2xl font-bold text-center'>
-                  {product?.name}
-                </h2>
+        <div className="px-5 py-8 md:p-10 ">
+            <div className="top flex flex-col gap-5 ">
+                <h2 className='text-2xl font-bold text-center '>
+                  {product?.name} hihihi
+                </h2> 
                 <p className='text-sm text-center'>
                   {product?.long_description}
                 </p>
             </div>
             <div className="bottom md:flex gap-8 pb-5">
                 <div className="left left-1">
-                    <img className='w-full h-full object-cover rounded-md' src="https://images.unsplash.com/photo-1595617795501-9661aafda72a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=650&q=40" alt="" />
+                    <img className='w-full h-full object-cover rounded-md' src={product?.poster} alt="" />
                 </div>
                 <div className="right flex-1 flex flex-col gap-5 pt-4 md:pt-0">
                     <h2 className='text-2xl font-semibold '>${product?.price}</h2>
                     <Rating rating={product?.rating}/>
                     <div className="flex gap-5 items-center">
-                        <span className=' uppercase bg-amber-50 text-amber-500 border py-1.5 px-3 rounded-md text-[10px] font-semibold'>${product?.best_seller}</span>
-                        <span className='uppercase bg-slate-100 text-emerald-600 border py-1.5 px-3 rounded-md text-[10px] font-semibold'>INSTOCK</span>
+                        {
+                          product?.best_seller ?
+                          <span className=' uppercase bg-amber-50 text-amber-500 border py-1.5 px-3 rounded-md text-[10px] font-semibold'>best_seller{product?.best_seller}</span>
+                          : ""
+                        }
+                        
+                        
+                        {product?.in_stock ? 
+                        <span className='uppercase bg-slate-100 text-emerald-600 border py-1.5 px-3 rounded-md text-[10px] font-semibold'>INSTOCK</span> 
+                        : 
+                        <span className='uppercase bg-slate-100 text-red-600 border py-1.5 px-3 rounded-md text-[10px] font-semibold'>OutofStock</span> 
+                        }
+                       
+                        
                         <span className='uppercase bg-slate-100 text-blue-500 border py-1.5 px-3 rounded-md text-[10px] font-semibold'>7MB</span>
                     </div>
                     <Link to='' className='py-2 px-3 bg-blue-600 text-sm cursor-pointer rounded-md w-fit text-white font-semibold'>Add to Cart +</Link>
