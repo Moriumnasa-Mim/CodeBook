c3580aebaf0f0d4befd28d66401cf0a68c0621f7
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2025-01-12 13:18:28 +0600

    Update Cartitem.jsx

--- CodeBook/src/components/shoppingcart/Cartitem.jsx
+++ CodeBook/src/components/shoppingcart/Cartitem.jsx
@@ -7,12 +7,12 @@ import { useCart } from "../../context/cartContext";
 const Cartitem = (product) => {
   /**7jan start */
 
-  const { cartList, total } = useCart();
+  const { cartList, total, clearCart } = useCart();
 
   /**7 jan close */
 
   const [cartCheck, setCartCheck] = useState(false);
-
+  const { removeToCart } = useCart()
   return (
     <div className="px-5 py-8 md:p-10 mx-auto bg-white shadow-md rounded-lg">
       <div className="flex justify-between items-center border-b pb-4 mb-4">
@@ -29,24 +29,33 @@ const Cartitem = (product) => {
       <div className="space-y-4 ">
         
         {cartList?.map((cartProduct) => (
-          <div key={cartProduct.id} product={cartProduct} 
+          <div 
+          key={cartProduct.id} 
+
           className="flex items-center justify-between p-4 border rounded-md">
             <img
-              src={product.poster}
+              src={cartProduct.poster}
               alt="Django"
               className="w-20 h-20 rounded-md object-cover"
             />
             <div className="flex-1 ml-4">
               <h3 className="text-lg font-medium text-gray-800">
-                {product.name}
+                {cartProduct.name}
               </h3>
             </div>
             <span className="text-lg font-semibold text-gray-700">
-              {product.price}
+              {cartProduct.price}
             </span>
-            <button className="ml-4 bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600">
+
+            <div onClick={()=>removeToCart(product)}
+            className="ml-4 bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600">
+            <button >
               Remove
             </button>
+            </div>
+
+            
+
           </div>
         ))}
       </div>
@@ -59,6 +68,16 @@ const Cartitem = (product) => {
         hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-gray-400"
         >
           Placeholder
+        </button>
+
+
+        <button
+          onClick={() => clearCart()}
+          type="button"
+          className="w-cover mt-3 bg-blue-700 text-white text-sm py-2 px-4 rounded-md 
+        hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-gray-400"
+        >
+          ClearCart
         </button>
 
         {cartCheck && <Cartcheckout setCartCheck={setCartCheck} />}
