5ecbbc91af0e88728f2f7de21432c503e01a9068
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2025-01-14 11:22:43 +0600

    Update Card.jsx

--- CodeBook/src/components/Card.jsx
+++ CodeBook/src/components/Card.jsx
@@ -1,12 +1,17 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React, { useState } from 'react'
 import { FaPlus } from "react-icons/fa6";
 import Rating from './rating';
 import { Link } from 'react-router';
 import { useCart } from '../context/cartContext';
 
 const Card = ({product}) => {
+
+//add to card to remove to card start
+const[inCart,setInCart]= useState(false)
+//add to card to remove to card end
+
 //add to card er logic start
   const{cartList, addToCart} = useCart();
   console.log("CartList",cartList)
@@ -19,14 +24,14 @@ const Card = ({product}) => {
       
       <Link to={`/product/${product.id}`}>
       <img className='w-full h-full object-cover rounded-md' src={product.poster} alt={product.name}/>
-      </Link>     
+      </Link>
       
       {product.best_seller? 
       <div className="absolute top-5 left-5 bg-orange-500 px-3 py-2 text-white rounded-md">
         <h2>Best Seller</h2>
       </div>:""
       }
-
+      
     </div>
     <div className="bottom flex flex-col gap3 px-3">
       <h2 className='text-2xl truncate text-gray-900  font-bold'>{product.name}</h2>
@@ -39,7 +44,10 @@ const Card = ({product}) => {
 
         <div className="flex items-center justify-between">
           <p className='text-3xl text-black font-semibold'>{product.price}</p>
-          <button onClick ={()=>addToCart(product)} className='flex items-center gap-2 bg-blue-500 text-white p-3  '>Add to cart <FaPlus /> 
+          <button onClick ={()=>addToCart(product)} 
+          className='flex items-center gap-2 bg-blue-500 text-white p-3  '>
+            Add to cart 
+          <FaPlus /> 
           </button>
         </div>
     </div>
