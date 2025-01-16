fbc50c0d52b02cc328c881ecbe0ac0f88c5d0087
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2025-01-16 13:58:03 +0600

    Add files via upload

---
+++ CodeBook/src/apiServices/authService.jsx
@@ -0,0 +1,21 @@
+//jwt token porte hobe
+export const signUp = async(authDetail)=>{
+
+    const requestOptions ={
+        method: "post",
+        headers: {"content-Type": "application/json"},
+        body: JSON.stringify(authDetail)
+    }
+
+    const response = await fetch(`http://localhost:800/register`, requestOptions)
+
+    const data = await response.json();
+
+
+    if(data.accessToken){
+        sessionStorage.setItem("token",JSON.stringify(data.accessToken));
+        sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
+    }
+    return data
+
+}
