5f54d7b28233439ba787f837e0663b5596d5e02a
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2025-01-21 12:35:04 +0600

    Update authService.jsx

--- CodeBook/src/apiServices/authService.jsx
+++ CodeBook/src/apiServices/authService.jsx
@@ -19,3 +19,24 @@ export const signUp = async(authDetail)=>{
     return data
 
 }
+
+export const login = async(authDetail)=>{
+
+    const requestOptions ={
+        method: "post",
+        headers: {"content-Type": "application/json"},
+        body: JSON.stringify(authDetail)
+    }
+
+    const response = await fetch(`http://localhost:800/login`, requestOptions)
+
+    const data = await response.json();
+
+
+    if(data.accessToken){
+        sessionStorage.setItem("token",JSON.stringify(data.accessToken));
+        sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
+    }
+    return data
+
+}
