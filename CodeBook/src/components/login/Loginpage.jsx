87e82af168e14edbfe835f1bc9ecb9851fa2de33
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2025-01-21 13:48:33 +0600

    Update Loginpage.jsx

--- CodeBook/src/components/login/Loginpage.jsx
+++ CodeBook/src/components/login/Loginpage.jsx
@@ -2,6 +2,8 @@
 /* eslint-disable no-unused-vars */
 import React, { useRef } from 'react'
 import { useNavigate } from 'react-router';
+import { login } from '../../apiServices/authService';
+
 
 const Loginpage = () => {
     const email = useRef();
@@ -34,12 +36,12 @@ const Loginpage = () => {
     className='space-y-4 md:space-y-6'>
       <div className="mb-4">
         <label htmlFor="email" className="block text-gray-600 mb-2">Your email</label>
-        <input type="email" id="email" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none 
+        <input ref={email} type="email" id="email" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none 
         focus:ring-2 focus:ring-blue-500" placeholder="name@company.com"/>
       </div>
       <div className="mb-6">
         <label htmlFor="password" className="block text-gray-600 mb-2">Password</label>
-        <input type="password" id="password" className="w-full px-4 py-2 border border-gray-300 rounded-md 
+        <input ref={password} type="password" id="password" className="w-full px-4 py-2 border border-gray-300 rounded-md 
         focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="••••••••"/>
       </div>
       <button type="submit" className="w-full bg-blue-700 text-white text-sm py-2 px-4 rounded-md hover:bg-blue-800 
