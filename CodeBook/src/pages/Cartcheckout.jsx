9698aad94314c637d5df2ae11bae02dc6d541081
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2025-01-27 12:49:29 +0600

    Update Cartcheckout.jsx

--- CodeBook/src/pages/Cartcheckout.jsx
+++ CodeBook/src/pages/Cartcheckout.jsx
@@ -62,7 +62,7 @@ const Cartcheckout = ({setCartCheck}) => {
                         text-gray-900 dark:text-white capitalize'>
                             email:
                         </label>
-                        <input type="email" value=""
+                        <input type="email" value={user?.email||'...@gmail.com'}
                         name='email' id='email' 
                         className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg
                         focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600
