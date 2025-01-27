6d549d63bfc3260a9e01e757ba886d236274cf98
Mim <76652494+Moriumnasa-Mim@users.noreply.github.com>
2025-01-27 12:50:01 +0600

    Update orderAuthServices.jsx

--- CodeBook/src/apiServices/orderAuthServices.jsx
+++ CodeBook/src/apiServices/orderAuthServices.jsx
@@ -2,7 +2,7 @@ const getSession =()=>{
     const token = JSON.parse(sessionStorage.getItem("token"))
     const cbid = JSON.parse(sessionStorage.getItem("cbid"))
 
-    return token, cbid
+    return {token, cbid}
 }
 export const getUser = async()=>{
     const browserData = getSession()
@@ -20,4 +20,37 @@ export const getUser = async()=>{
 
     return data
 
+}
+
+
+
+
+
+
+
+
+
+export const createOrder = async(cartList, SiToptal, user, cardDetail)=>{
+    const browserData = getSession()
+
+    const order = {
+        cartList:cartList,
+        amount_paid: total,
+        quantity:cartList.length,
+        cardDetail:cardDetail,
+        user:{
+            name: user.name,
+            email:user.email,
+            id:user.id,
+        },
+    };
+
+    const requestOptions = {
+        method:"POST",
+        headers: {"Content-Type": "application/json", Authorization:`Bearer ${browserData.token}`},
+        body: JSON.stringify(order)
+    }
+
+    const response = await fetch(`http://localhost:800/orders`, requestOptions);
+    const data = await response.json();
 }
